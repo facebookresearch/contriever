9abbba384b495496555570d653c3b5d585130d3c
gizacard <gizacard@fb.com>
2022-01-24 17:03:19 +0100

    Update eval_beir.py
    style formatting

--- eval_beir.py
+++ eval_beir.py
@@ -23,44 +23,44 @@ import src.dist_utils as dist_utils
 logger = logging.getLogger(__name__)
 
 
-def main(opt):
+def main(args):
 
-    src.slurm.init_distributed_mode(opt)
+    src.slurm.init_distributed_mode(args)
     src.slurm.init_signal_handler()
 
-    os.makedirs(opt.output_dir, exist_ok=True)
+    os.makedirs(args.output_dir, exist_ok=True)
 
-    logger = utils.init_logger(opt)
-    logger.info(f"Loading model from {opt.model_name_or_path}")
+    logger = utils.init_logger(args)
+    logger.info(f"Loading model from {args.model_name_or_path}")
 
-    tokenizer = transformers.AutoTokenizer.from_pretrained(opt.model_name_or_path)
-    model = src.contriever.Contriever.from_pretrained(opt.model_name_or_path)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_name_or_path)
+    model = src.contriever.Contriever.from_pretrained(args.model_name_or_path)
 
     model = model.cuda()
 
     logger.info("Start indexing")
 
     ndcg, _map, recall, precision, mrr, recall_cap, hole = src.beir_utils.evaluate_model(
-        query_encoder=model, 
+        query_encoder=model,
         doc_encoder=model,
-        tokenizer=tokenizer, 
-        dataset=opt.dataset,
-        batch_size=opt.per_gpu_batch_size,
-        norm_query=opt.norm_query,
-        norm_doc=opt.norm_doc,
+        tokenizer=tokenizer,
+        dataset=args.dataset,
+        batch_size=args.per_gpu_batch_size,
+        norm_query=args.norm_query,
+        norm_doc=args.norm_doc,
         is_main=dist_utils.is_main(),
-        split='dev' if opt.dataset=='msmarco' else 'test',
-        metric=opt.metric,
-        beir_data_path=opt.beir_data_path,
+        split='dev' if args.dataset=='msmarco' else 'test',
+        metric=args.metric,
+        beir_data_path=args.beir_data_path,
     )
 
     if dist_utils.is_main():
-        logger.info(opt.dataset + ' ' + str(ndcg))
-        logger.info(opt.dataset + ' ' + str(recall))
-        logger.info(opt.dataset + ' ' + str(precision))
-        logger.info(opt.dataset + ' ' + str(mrr))
-        logger.info(opt.dataset + ' ' + str(recall_cap))
-        logger.info(opt.dataset + ' ' + str(hole))
+        logger.info(args.dataset + ' ' + str(ndcg))
+        logger.info(args.dataset + ' ' + str(recall))
+        logger.info(args.dataset + ' ' + str(precision))
+        logger.info(args.dataset + ' ' + str(mrr))
+        logger.info(args.dataset + ' ' + str(recall_cap))
+        logger.info(args.dataset + ' ' + str(hole))
 
 
 if __name__ == '__main__':
@@ -73,10 +73,10 @@ if __name__ == '__main__':
     parser.add_argument("--per_gpu_batch_size", default=128, type=int, help="Batch size per GPU/CPU for indexing.")
     parser.add_argument("--output_dir", type=str, default="./my_experiment", help="Output directory")
     parser.add_argument("--model_name_or_path", type=str, help="Model name or path")
-    parser.add_argument("--metric", type=str, default='dot', 
+    parser.add_argument("--metric", type=str, default="dot",
         help="Metric used to compute similarity between two embeddings")
-    parser.add_argument('--norm_query', action='store_true', help="Normalize query representation")
-    parser.add_argument('--norm_doc', action='store_true', help="Normalize document representation")
+    parser.add_argument("--norm_query", action="store_true", help="Normalize query representation")
+    parser.add_argument("--norm_doc", action="store_true", help="Normalize document representation")
 
     parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
     parser.add_argument("--main_port", type=int, default=-1, help="Main port (for multi-node SLURM jobs)")
@@ -84,3 +84,4 @@ if __name__ == '__main__':
 
     args, _ = parser.parse_known_args()
     main(args)
+    
